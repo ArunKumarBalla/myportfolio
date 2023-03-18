9708df3252f3fe51c73e792f12d8e9b618293d52
ArunKumarBalla <arunkumarballa503@gmail.com>
2023-03-19 00:01:37 +0530

    modifed files
    modifed css and javascript files in git hub

--- js/script.js
+++ js/script.js
@@ -5,6 +5,35 @@ $(document).ready(function(){
         $('header').toggleClass('toggle');
     });
 
-    $(window).on('scroll ')
+    $(window).on('scroll load',function(){
+
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
+
+        if($(window).scrollTop() > 0){
+            $('.top').show();
+        }else{
+            $('.top').hide();
+        }
+
+    });
+
+    // smoth scrollong
+
+    $('a[href*="#"]').on('click',function(e){
+
+        e.preventDefault();
+
+        $('html, body').animate({
+
+            scrollTop : $($(this).attr('href')).offset().top,
+
+        },
+            500,
+            'linear'
+
+        );
+
+    });
 
 });
